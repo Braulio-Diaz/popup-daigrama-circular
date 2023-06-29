64494388119ccfda2100fe8f0a961a44a014cf89
Braulio Díaz <braulio.diaz.gajardo@ciisa.cl>
2023-06-29 04:07:29 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,126 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js.js"></script>
+    <title>Diagrama POP-UP</title>
+</head>
+<body>
+<main class="container">
+  <div class="row g-3">
+    <div class="col-md-4 py-5"></div>
+    <div class="col-md-4 py-5">
+    <div class="row">
+      <ul class="sliceWrapper">
+        <li class="li">
+          <a href="#modal" class="btn-open-popup-red">
+            <div class="slice"></div>
+          </a>
+        </li>
+        <li class="li">>
+          <a href="#modal2" class="btn-open-popup-mustard">
+            <div class="slice"></div>
+          </a>
+        </li>
+        <li class="li">
+          <a href="#modal3" class="btn-open-popup-skyblue">
+            <div class="slice"></div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+<div class="icono">
+  <a href="#modal2" class="btn-open-popup-mustard">
+    <img src="https://cdn-icons-png.flaticon.com/128/1253/1253976.png" alt="" height="100px" width="100px">
+  </a>
+</div>
+<div class="icono2">
+  <a href="#modal" class="btn-open-popup-red">
+    <img src="img/avatar.png" alt="" height="100px" width="100px">
+  </a>
+</div>
+<div class="icono3">
+  <a href="#modal3" class="btn-open-popup-skyblue">
+    <img src="img/save.png" alt="" height="100px" width="100px">
+  </a>
+</div>
+<div class="titulo">
+  <a href="#modal2" class="btn-open-popup-mustard">
+    <h2 style="color:black;">Residuos</h2>
+  </a>
+</div>
+<div class="titulo2">
+  <a href="#modal" class="btn-open-popup-red">
+    <h2 style="color:black;">Tú nos importas</h2>
+  </a>
+</div>
+<div class="titulo3">
+  <a href="#modal3" class="btn-open-popup-skyblue">
+    <h2 style="color:black;">Prácticas <br> sostenibles</h2>
+  </a>
+</div>
+    <div class="container-all" id="modal">
+        <div class="popup">
+            <div class="img"></div>
+            <div class="container-text">
+                <h1>Tú nos importas</h1><br>
+                <p>¡Hey! ¿No te parece increíble que vivamos en un mundo 
+                    tan conectado en el que todo lo que hacemos puede 
+                    tener un impacto en las personas que nos rodean? En 
+                    A’Mangiare nos inspira mucho saber que nuestras acciones 
+                    pueden tener un efecto positivo en la vida de los demás y 
+                    que, al mismo tiempo, las interacciones con ellos nos 
+                    motivan a buscar un estilo de vida mejor para todos. 
+                    ¿No te parece emocionante?</p><br>
+                <p> -	SALUD Y BIENESTAR NUTRICIONAL <br>
+                    -	PROVEEDORES DE ALTA CALIDAD <br>
+                    -	MÁXIMA SATISFACCIÓN DEL SERVICIO </p>
+            </div>
+            <a href="#" class="btn-close-popup">X</a>
+        </div>
+    </div>
+    <div class="container-all" id="modal2">
+        <div class="popup">
+            <div class="img2"></div>
+            <div class="container-text">
+                <h1>Residuos</h1><br>
+                <p>¡Hola! En nuestro compromiso con el cuidado del medio ambiente, 
+                    estamos haciendo todo lo posible para reducir nuestro impacto 
+                    ambiental en la entrega de nuestras Colaciones Frías y Desayunos 
+                    Box & Brunch. Algunas de nuestras medidas son </p><br>
+                <p> -	MATERIAL RECICLADO <br>
+                    -	ENVASES Y EMBALAJES BIODEGRADABLES <br>
+                    -	BUEN CONTROL DE INVENTARIO (MENOS DESECHOS)</p>
+            </div>
+            <a href="#" class="btn-close-popup">X</a>
+        </div>
+    </div>
+    <div class="container-all" id="modal3">
+        <div class="popup">
+            <div class="img3"></div>
+            <div class="container-text">
+                <h1>Prácticas sostenibles</h1><br>
+                <p>¿Sabías que constantemente intentamos implementar nuevas prácticas 
+                    sostenibles para minimizar nuestra huella de carbono y promover 
+                    un futuro más saludable para nuestro planeta?, eso nos motiva a 
+                    innovar por tu bien y el de nuestro ecosistema</p><br>
+                <p> -   DIGITALIZACIÓN (AHORRO DE PAPEL) <br>
+                    -	PUNTOS LIMPIOS <br>
+                    -	INCENTIVAR EL COMPORTAMIENTO ECO AMIGABLE A NUESTROS CLIENTES <br> 
+                    -	GESTIÓN EFICIENTE DE RESIDUOS 
+                    </p>
+            </div>
+            <a href="#" class="btn-close-popup">X</a>
+        </div>
+    </div>
+    <div class="col-md-4 py-5"></div>
+  </div>
+</main>
+</body>
+</html>
+
